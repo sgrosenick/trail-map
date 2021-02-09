315ade70b334ee3d4168a711a303c0af628181a6
Samuel Grosenick <srgrosenick@gmail.com>
2021-02-08 19:16:46 -0900

    set up map

--- blazormap/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ blazormap/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,70 +13,70 @@ namespace blazormap.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/Users/samuelgrosenick/Projects/blazormap/blazormap/_Imports.razor"
+#line 1 "/Users/samuelgrosenick/Documents/Projects/blazormap/blazormap/_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/samuelgrosenick/Projects/blazormap/blazormap/_Imports.razor"
+#line 2 "/Users/samuelgrosenick/Documents/Projects/blazormap/blazormap/_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/samuelgrosenick/Projects/blazormap/blazormap/_Imports.razor"
+#line 3 "/Users/samuelgrosenick/Documents/Projects/blazormap/blazormap/_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/samuelgrosenick/Projects/blazormap/blazormap/_Imports.razor"
+#line 4 "/Users/samuelgrosenick/Documents/Projects/blazormap/blazormap/_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/samuelgrosenick/Projects/blazormap/blazormap/_Imports.razor"
+#line 5 "/Users/samuelgrosenick/Documents/Projects/blazormap/blazormap/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/samuelgrosenick/Projects/blazormap/blazormap/_Imports.razor"
+#line 6 "/Users/samuelgrosenick/Documents/Projects/blazormap/blazormap/_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/Users/samuelgrosenick/Projects/blazormap/blazormap/_Imports.razor"
+#line 7 "/Users/samuelgrosenick/Documents/Projects/blazormap/blazormap/_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/Users/samuelgrosenick/Projects/blazormap/blazormap/_Imports.razor"
+#line 8 "/Users/samuelgrosenick/Documents/Projects/blazormap/blazormap/_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/Users/samuelgrosenick/Projects/blazormap/blazormap/_Imports.razor"
+#line 9 "/Users/samuelgrosenick/Documents/Projects/blazormap/blazormap/_Imports.razor"
 using blazormap;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/Users/samuelgrosenick/Projects/blazormap/blazormap/_Imports.razor"
+#line 10 "/Users/samuelgrosenick/Documents/Projects/blazormap/blazormap/_Imports.razor"
 using blazormap.Shared;
 
 #line default
--- blazormap/wwwroot/js/map.js
+++ blazormap/wwwroot/js/map.js
@@ -1,10 +1,10 @@
-﻿using System;
-namespace blazormap.wwwroot.js
-{
-	public class map
-	{
-		public map()
-		{
-		}
-	}
+﻿function loadMap(element) {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoic2dyb3NlbmljayIsImEiOiJjaWZ1bDFtZW0yMzZ2djBtMGR3dDR4Z2ZhIn0.7rppXXzXGN9OiQ7WhNA9vw';
+
+    var map = new mapboxgl.Map({
+        container: element,
+        style: 'mapbox://styles/mapbox/dark-v10',
+        center: [-150.0, 61.16], 
+        zoom: 7
+    });
 }
